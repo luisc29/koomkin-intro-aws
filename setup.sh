063234a5b0f05b04e0d17fcbf3693d18bc4198c6
Luis <lcruz@koomkin>
2018-10-18 17:44:50 -0500

    Comandos para montar el servicio

--- setup.sh
+++ setup.sh
@@ -1,9 +1,6 @@
 # instala dependencias 
 sudo apt-get -y update
-sudo apt-get -y install python3 python3-pip nginx git nodejs npm
-
-cp server_test.py
-sudo cp dummy.service /etc/systemd/system/dummy.service
+sudo apt-get -y install python3 python3-pip nginx git nodejs npm virtualenv
 
 # crea un env para python
 pip3 install virtualenv
@@ -12,3 +9,12 @@ mkdir server-data
 virtualenv server-env
 source server-env/bin/activate
 pip install flask request
+
+# prueba el servidor
+cd server-data
+python server_dummy.py
+
+# prepara el servicio
+sudo cp dummy.service /etc/systemd/system/dummy.service
+sudo systemctl start dummy
+sudo systemctl enable dummy
